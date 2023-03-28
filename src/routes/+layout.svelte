6385e806bb65bc47084ed51fd38235cb519c7a20
Noran Raskin <n.raskin@campus.tu-berlin.de>
2023-03-28 19:29:08 +0200

    Add general layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Header from "./header.svelte";
+    import Footer from "./footer.svelte";
+</script>
+
+<main class="bg-white dark:bg-zinc-800 dark:text-white min-h-screen flex flex-col">
+        <Header/>
+    <main class="px-8">
+        <slot />
+    </main>
+    
+        <Footer/>
+</main>
