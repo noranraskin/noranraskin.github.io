3afdb361f0083e600c82e9efff9360a44733917e
Noran Raskin <n.raskin@campus.tu-berlin.de>
2023-08-02 18:10:29 +0200

    Add sample blog post

---
+++ src/routes/blog/[slug]/2023-08-02.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" context="module">
+	export const title = 'First Blog Post';
+	export const slug = 'first-blog-post';
+	export const date = new Date(2023, 8, 2);
+	export const categories = [];
+	export const published = true;
+</script>
+
+First blog post. This is a test post to see if the blog works. It does. Lorem ipsum dolor sit amet,
+consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
