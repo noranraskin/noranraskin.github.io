e423ced44f7f2655656ba2a843018ef11861cae8
Noran Raskin <n.raskin@campus.tu-berlin.de>
2023-08-02 18:22:13 +0200

    Create 404 route so github pages picks up the 404.html

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let loadingDots = '';
+	onMount(() => {
+		setInterval(() => {
+			if (loadingDots.length < 4) {
+				loadingDots += '.';
+			} else {
+				loadingDots = '';
+			}
+		}, 500);
+		setTimeout(() => {
+			// let path = $page.url.pathname;
+			window.location.href = '/';
+		}, 3000);
+	});
+</script>
+
+<main class="flex flex-row">
+	<div class="flex flex-col grow self-center pt-40">
+		<h1 class="text-9xl font-bold self-center">404</h1>
+		<h2 class="text-5xl self-center">Not found</h2>
+		<p class="self-center p-3">You will be redirected shortly{loadingDots}</p>
+	</div>
+</main>
